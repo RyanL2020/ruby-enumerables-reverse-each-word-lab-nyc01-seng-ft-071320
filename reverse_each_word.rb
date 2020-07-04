297fa6c8f5e9366aa5492312937ed5a6a4ef9e24
Ryan Locascio <loc.fitness1@gmail.com>
2020-07-04 16:07:17 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
   array = string.split(" ")
   return_array = []
   array.each do |string|
-    return array << string.reverse 
-end 
+    return array << string.reverse
+end
  return_array.join(" ")
  end 
